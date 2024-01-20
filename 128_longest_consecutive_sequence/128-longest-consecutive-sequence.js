f6fa5689d8af66038bdd17b083fdae352f45cc3a
Logan Matheny <loganpaulmatheny@gmail.com>
2024-01-20 18:52:29 -0500

    Set up problem 128

---
+++ 128_longest_consecutive_sequence/128-longest-consecutive-sequence.js
@@ -0,0 +1,13 @@
+// Understanding 
+// You have an unsorted array and you need to determine if the numbers are consecutive and then return the length of them
+
+// Methods 
+// I think the ruby next method would be really helpful
+
+
+// Pseudocode 
+// sort the array
+// create a counter 
+// loop through and increment the counter as long as the next element is 1 larger than the previous
+// if it's not add the counter to the set and reset it to 0
+// get the maximum number from the set 
