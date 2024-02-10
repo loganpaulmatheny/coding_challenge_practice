0bf93c762b25bab1bc049b27ffeef86efb575f6f
Logan Matheny <loganpaulmatheny@gmail.com>
2024-02-10 10:47:34 -0500

    Save changes to challenges

---
+++ 15threeSum/15threeSum.js
@@ -0,0 +1,16 @@
+
+
+// Understanding 
+
+// We want to iterate through an array and find sets of numbers that add up to 0
+// we cannot 'double count' an index e.g. if arr[1] = -2, we could not use that twice with 4 to get a sum equal to 0 
+
+// Methods and Ideas
+// we should use pointers 
+// we could...have a pointer at the beginning and the end
+// and then slowly move the pointer at the end forward iterating through the midlle part of the array
+
+
+// Pseudocode 
+// create three pointers 
+// 
