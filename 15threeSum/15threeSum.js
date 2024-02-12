195262025dfbf9b7b0ffc6c3a78c8708e799e11d
Logan Matheny <loganpaulmatheny@gmail.com>
2024-02-12 13:42:50 -0500

    Stash commits

--- 15threeSum/15threeSum.js
+++ 15threeSum/15threeSum.js
@@ -8,9 +8,28 @@
 // Methods and Ideas
 // we should use pointers 
 // we could...have a pointer at the beginning and the end
-// and then slowly move the pointer at the end forward iterating through the midlle part of the array
+// and then slowly move the pointer at the end forward iterating through the middle part of the array 
 
 
 // Pseudocode 
 // create three pointers 
-// 
+// iterate through starting at positions 1, 2 and last 
+// see if there's a combo that equals zero
+// move the first, middle, and last pointers 
+// do this until j = k
+
+
+function threeSum(nums) {
+  for (let i = 0, j = i + 1, k = nums.length - 1; j == k; i++, j++, k--) {
+      console.log(nums[i] + nums[j] + nums[k]);
+      console.log('run ' + i )
+      if (nums[i] + nums[j] + nums[k] == 0) {
+          console.log("found a solution")
+          return [i, j, k];
+      }
+  }
+  return [];
+};
+
+let nums = [-1,0,1,2,-1,-4];
+console.log(threeSum(nums))
